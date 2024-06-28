d2ab18557e2b2005f1337bd8f5599a042a10af05
celsoaquino <celso.aquino@ymail.com>
2024-06-28 19:05:34 -0300

    Initial commit

---
+++ src/main/java/com/celsoaquino/diochallengejava/DioChallengeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.celsoaquino.diochallengejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioChallengeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioChallengeJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/celsoaquino/diochallengejava/DioChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celsoaquino.diochallengejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioChallengeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
